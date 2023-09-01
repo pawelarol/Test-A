3304a0dac2ed9bf0fa499829f1a80113520f8aed
pawelarol <123151436+pawelarol@users.noreply.github.com>
2023-09-01 19:50:37 +0200

    Added new class

---
+++ TestGit/src/Class.java
@@ -0,0 +1,14 @@
+public class Class {
+
+    private String name;
+    private int age;
+    private char g;
+    private boolean a;
+    private long days;
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
